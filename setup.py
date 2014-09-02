74f3e40033f5cfe811a36bc1ca4a044bd49142f9
Low Kian Seong <kianseong@gmail.com>
2014-09-02 11:19:50 +0800

    Update setup.py
    Adding more metadata to setup.py for a more complete listing on `pypi`

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "django-emailredirect",
     version = "0.2",
+    description="Simple Django email backend",
+    url="https://github.com/marimore/django-emailredirect",
     packages = find_packages(),
 )
